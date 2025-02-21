708340b4fe84e1ccc989f1a41d312531ff6ee2ae
rza628 <ryanzhang2468@gmail.com>
2025-02-20 20:29:57 -0500

    pandas df for 2024 fpl aggregate data

--- players.py
+++ players.py
@@ -1,6 +1,15 @@
 import requests
 import sys
+import pandas as pd
+#import kagglehub
 sys.stdout.reconfigure(encoding='utf-8') # needs utf-8 for fetched data
+
+
+# Download latest version
+""" path = kagglehub.dataset_download("meraxes10/fantasy-premier-league-2024")
+
+print("Path to dataset files:", path) """
+
 
 def fpl_api():
     url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
@@ -19,4 +28,6 @@ positions_map = {pos["id"] : pos["singular_name"] for pos in retreived_fpl_data[
 teams_map = {team["id"]: team["name"] for team in retreived_fpl_data["teams"]}
 name_id_players_map = {f'{player["first_name"]} {player["second_name"]}': player["id"] for player in retreived_fpl_data["elements"]}
 id_stats_players_map = {player["id"] : player for player in retreived_fpl_data["elements"]}
-print(id_stats_players_map)
+#print(positions_map)
+fpl_2024_data = pd.read_csv('players.csv')
+print(fpl_2024_data.head())
